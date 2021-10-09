391242dfbe7f11b81d914ac0d0b7fe78927b0a55
neko2048 <show.999991@gmail.com>
2021-10-09 20:22:34 +0800

    optimize init.sh

--- init.sh
+++ init.sh
@@ -1,30 +1,27 @@
 #!bin/bash
-echo 'type the folder name:'
+echo "type the folder name:"
 read fname
 
 # initialize folder
-echo 'check needed folder'
+echo "===== check needed folder ====="
+fmain="./$fname"
+fdata="/home/atmenu10246/VVM/DATA/$fname"
 
+
+# checking dataset existence
+[ ! -d "$fdata" ] && echo "no such folder" && exit 1
+
+# checking local drawing folder existence
+[ ! -d "$fmain" ] && mkdir "$fmain" && echo "mkdir $fmain" || echo "$fmain folder exists"
+
+
+# folders for drawing variables
 fthermody="./$fname/thermodynamic"
-if [ ! -d "$fthermody" ]; then
-    mkdir $fname/thermodynamic
-    echo "make $fthermody/"
-else
-    echo "$fthermody folder exists"
-fi
+[ ! -d "$fthermody" ] && mkdir $fname/thermodynamic && echo "make $fthermody/" || echo "$fthermody folder exists"
 
 fdiag="./$fname/diag"
-if [ ! -d "$fdiag" ]; then
-    mkdir $fname/diag
-    echo "make $fdiag/"
-else
-    echo "$fdiag folder exists"
-fi
+[ ! -d "$fdiag" ] && mkdir $fname/diag && echo "make $fdiag/" || echo "$fdiag folder exists"
 
 fdynamic="./$fname/dynamic"
-if [ ! -d "$fdynamic" ]; then
-    mkdir $fname/dynamic
-    echo "make $fdynamic/"
-else
-    echo "$fdynamic folder exists"
-fi
+[ ! -d "$fdynamic" ] && mkdir $fname/dynamic && echo "make $fdynamic/" || echo "$fdynamic folder exists"
+# ============================
