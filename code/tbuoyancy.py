ee760e72157ada11ea346244d5ff717286a3c416
neko2048 <show.999991@gmail.com>
2021-10-16 16:04:23 +0800

    accurate grid

--- code/tbuoyancy.py
+++ code/tbuoyancy.py
@@ -50,27 +50,25 @@ def draw_cloud(profile):
     cmap = nclcmap('BrownBlue12')
     colors = cm.Blues(np.linspace(0.3, 1))
     cmap = LinearSegmentedColormap.from_list('name', colors)
-    qr = cut_edge(thmo["qr"], set_axisarg)[1:, 1:, xarg]
+    qr = cut_edge(thmo["qr"], set_axisarg)[:, :, xarg]
     qr = ma.masked_array(qr, qr<=1e-6)#5e-6)
-    pcolormesh(yc, zc, qr, cmap=cmap, vmin=0, vmax=0.005)
-    colorbar()
+    pcolormesh(yb, zb, qr, cmap=cmap, vmin=0, vmax=0.005)
+    #colorbar()
     # cloud
     colors = cm.Greys(np.hstack([np.linspace(0.5, 0.75, 95)]))
     cmap = LinearSegmentedColormap.from_list('name', colors)
-    qc = cut_edge(thmo["qc"], set_axisarg)[1:, 1:, xarg]
+    qc = cut_edge(thmo["qc"], set_axisarg)[:, :, xarg]
     qc = ma.masked_array(qc, qc<=0)
-    pcolormesh(yc, zc, qc, cmap=cmap, vmin=0, vmax=0.005)
-    colorbar()
+    pcolormesh(yb, zb, qc, cmap=cmap, vmin=0, vmax=0.005)
+    #colorbar()
 
     # wind information
-    w = (cut_edge(dyna["w"], set_axisarg)[1:] + \
-        cut_edge(dyna["w"], set_axisarg)[:-1]) / 2
-    yy, zz = np.meshgrid(yc, czc)
-    pw = w[:, :, xarg] > 0
-    quiver(yy[pw], zz[pw], 0, w[:, :, xarg][pw], color='red', alpha=0.2)
-    nw = w[:, :, xarg] < 0 
-    quiver(yy[nw], zz[nw], 0, w[:, :, xarg][nw], color='blue', alpha=0.2)
-
+    y, z = np.meshgrid(yc, zz)
+    w = cut_edge(dyna["w"], set_axisarg)[:, :, xarg]
+    pw = w > 0
+    quiver(y[pw], z[pw], 0, w[pw], color='red', alpha=0.2)
+    nw = w < 0 
+    quiver(y[nw], z[nw], 0, w[nw], color='blue', alpha=0.2)
 
     title("TIME: {T:06d}".format(T=i*2))
     savefig("cloud_{T:06d}.jpg".format(T=i), dpi=200)
@@ -80,22 +78,20 @@ def draw_buoyancy(profile, type):
     xarg = np.argmin(np.abs(xc  - profile))
     # buoyancy
     if type == "tv":
-        buoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg)[1:]
-        contourf(yc, czc, buoyancy[:, :, xarg], cmap='coolwarm', levels=np.arange(-0.015, 0.015, 0.001))
+        buoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg)[:]
+        contourf(yc, zc, buoyancy[:, :, xarg], cmap='coolwarm', levels=np.arange(-0.015, 0.015, 0.001))
     else:
-        buoyancy = gen_buoyancy(data=thmo, edge_arg=set_axisarg)[1:]
-        contourf(yc, czc, buoyancy[:, :, xarg], cmap='coolwarm', levels=np.arange(-0.15, 0.15, 0.01))
+        buoyancy = gen_buoyancy(data=thmo, edge_arg=set_axisarg)[:]
+        contourf(yc, zc, buoyancy[:, :, xarg], cmap='coolwarm', levels=np.arange(-0.15, 0.15, 0.01))
     colorbar()
 
     # vertical velocity
-    w = (cut_edge(dyna["w"], set_axisarg)[1:] + \
-        cut_edge(dyna["w"], set_axisarg)[:-1]) / 2
-
-    yy, zz = np.meshgrid(yc, czc)
-    pw = w[:, :, xarg] > 0
-    quiver(yy[pw], zz[pw], 0, w[:, :, xarg][pw], color='red', alpha=0.2)
-    nw = w[:, :, xarg] < 0 
-    quiver(yy[nw], zz[nw], 0, w[:, :, xarg][nw], color='blue', alpha=0.2)
+    y, z = np.meshgrid(yc, zz)
+    w = cut_edge(dyna["w"], set_axisarg)[:, :, xarg]
+    pw = w > 0
+    quiver(y[pw], z[pw], 0, w[pw], color='red', alpha=0.2)
+    nw = w < 0 
+    quiver(y[nw], z[nw], 0, w[nw], color='blue', alpha=0.2)
 
     title("X: {X} | TIME: {T:06d}".format(X=profile, T=i*2))
     savefig("buoyancy_tv{T:06d}.jpg".format(T=i), dpi=200)
@@ -103,10 +99,8 @@ def draw_buoyancy(profile, type):
 
 def draw_srf_rain():
     # surface rain 
-    qr = (cut_edge(thmo["qr"], set_axisarg)[1, :, :] + \
-          cut_edge(thmo["qr"], set_axisarg)[0, :, :]) / 2
+    qr = cut_edge(thmo['qr'], set_axisarg)[0, :, :]
     qr = ma.masked_array(qr, qr<0)
-
     cmap = nclcmap('BrownBlue12')
     #colors = cm.Blues(np.linspace(0.5, 1, 90))
     cmap = LinearSegmentedColormap.from_list('name', [(0, "#bc763c"), (0.1, "#ffffff"), (0.2, "#b5cbe6"), (1.0, "#007bbb")])
@@ -123,18 +117,15 @@ def draw_srf_rain():
     clf()
 
 
-def draw_cwv():
-    cqv = cut_edge(thmo["qv"], set_axisarg)[1:, 1:, 1:]
-    gradx = np.tile((xc[1:] - xc[:-1])[np.newaxis, np.newaxis, :], reps=(len(czc), len(yc)-1, 1))
-    grady = np.tile((yc[1:] - yc[:-1])[np.newaxis, :, np.newaxis], reps=(len(czc), 1, len(xc)-1))
-    gradz = np.tile((zc[1:] - zc[:-1])[:, np.newaxis, np.newaxis], reps=(1, len(yc)-1, len(xc)-1))
+def draw_wcv():
+    cqv = cut_edge(thmo["qv"], set_axisarg)
+    gradz = np.tile((zb[1:] - zb[:-1])[:, np.newaxis, np.newaxis], reps=(1, len(yc), len(xc)))
     # gradient z start from surface to heights
-    # gradient shape: (k-1, j-1, i-1)
-    grid_density = np.tile(cdensity[:, np.newaxis, np.newaxis], reps=(1, len(yc)-1, len(xc)-1))
+    grid_density = np.tile(denc[:, np.newaxis, np.newaxis], reps=(1, len(yc), len(xc)))
     weight = gradz * grid_density # shape: (k-1, j-1, i-1)
 
-    cwv = np.sum(weight * cqv, axis=(0)) # column water vapor, shape: (k-1, j-1, i-1)
-    pcolormesh(xc, yc, cwv, vmin=50, vmax=70)
+    wcv = np.sum(weight * cqv, axis=(0)) # column water vapor, shape: (k, j, i)
+    pcolormesh(xb, yb, wcv, vmin=50, vmax=70)
     colorbar()
 
     # cloud existence
@@ -142,23 +133,23 @@ def draw_cwv():
     Eqc = np.array(Eqc, dtype=int)
     contour(xc, yc, Eqc, levels=[1], colors='black')
     title("Column Water Vapor [" +  r"$kg/m^2$" + "] | T: {T:06d}".format(T=i*2))
-    savefig('cwv_{T:06d}.jpg'.format(T=i), dpi=200)
+    savefig('wcv_{T:06d}.jpg'.format(T=i), dpi=200)
     clf()
 
 def draw_xycoreshell():
-    tvbuoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg)[1:, 1:, 1:] # shape: (k-1, j-1, i-1)
-    w = cut_edge(dyna["w"], set_axisarg)[:, 1:, 1:] # choose for xy now
-    cw = (w[1:] + w[:-1]) / 2 # z center at one with theta
-    qc = cut_edge(thmo["qc"], set_axisarg)[1:, 1:, 1:]
+    tvbuoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg) # shape: (k, j, i)
+    w = cut_edge(dyna["w"], set_axisarg) # choose for xy now
+    wc = np.array([w[0]] + [xw for xw in (w[1:]+w[:-1])/2]) # z level same as the one with theta
+    qc = cut_edge(thmo["qc"], set_axisarg)
 
     # core: a saturated volume that is positively buoyant and moving upward
     colors = cm.Reds(np.hstack([np.linspace(0.5, 1)]))
     cmap = LinearSegmentedColormap.from_list('name', colors)
-    topmask_core = np.logical_and(tvbuoyancy > 0, cw > 0)
+    topmask_core = np.logical_and(tvbuoyancy > 0, wc > 0)
     topmask_core = np.logical_and(topmask_core, qc > 0)
     xytopmask_core = np.sum(topmask_core, axis=(0))
     xytopmask_core = np.ma.masked_array(xytopmask_core, xytopmask_core<=0)
-    pcolormesh(xc, yc, xytopmask_core, vmin=1, vmax=20, cmap=cmap)
+    pcolormesh(xb, yb, xytopmask_core, vmin=1, vmax=20, cmap=cmap)
     colorbar()
 
     # shell: saturated part but not core
@@ -167,7 +158,7 @@ def draw_xycoreshell():
     topmask_shell = np.array(qc > 0, dtype=int) - np.array(topmask_core, dtype=int)
     xytopmask_shell = np.sum(topmask_shell, axis=(0))
     xytopmask_shell = np.ma.masked_array(xytopmask_shell, xytopmask_shell<=0)
-    pcolormesh(xc, yc, xytopmask_shell, vmin=1, cmap=cmap, alpha=.5)
+    pcolormesh(xb, yb, xytopmask_shell, vmin=1, cmap=cmap, alpha=.5)
 
 
     title("Core & Shell Top View | T: {T:06d}".format(T=i*2))
@@ -176,15 +167,15 @@ def draw_xycoreshell():
 
 def draw_yzcoreshell(profile):
     xarg = np.argmin(np.abs(xc - profile))
-    tvbuoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg)[1:, 1:, xarg] # shape: (k-1, j-1, i-1)
-    w = cut_edge(dyna["w"], set_axisarg)[:, 1:, xarg] # choose for xy now
-    cw = (w[1:] + w[:-1]) / 2 # z center at one with theta
-    qc = cut_edge(thmo["qc"], set_axisarg)[1:, 1:, xarg]
+    tvbuoyancy = gen_tv_buoyancy(data=thmo, edge_arg=set_axisarg)[:, :, xarg] # shape: (k-1, j-1, i-1)
+    w = cut_edge(dyna["w"], set_axisarg)[:, :, xarg] # choose for xy now
+    wc = np.array([w[0]] + [xw for xw in (w[1:]+w[:-1])/2]) # z center at one with theta
+    qc = cut_edge(thmo["qc"], set_axisarg)[:, :, xarg]
 
     # core: a saturated volume that is positively buoyant and moving upward
     colors = cm.Reds(np.hstack([np.linspace(0.5, 0.75)]))
     cmap = LinearSegmentedColormap.from_list('name', colors)
-    mask_core = np.logical_and(tvbuoyancy > 0, cw > 0)
+    mask_core = np.logical_and(tvbuoyancy > 0, wc > 0)
     mask_core = np.logical_and(mask_core, qc > 0)
     mask_core = np.ma.masked_array(mask_core, mask_core<=0)
     pcolormesh(yc, zc, mask_core, cmap=cmap, vmin=0, vmax=1)
@@ -213,9 +204,11 @@ if __name__ == "__main__":
     thmo = load_data(case_name, "Thermodynamic", idx=tidx)
     dyna = load_data(case_name, "Dynamic", idx=tidx)
     # gloabl variable #
-    xc, yc = np.array(thmo['xc']), np.array(thmo['yc']), 
-    zc = np.array(thmo['zc'])
-    density = np.loadtxt("../constants/diurnal_prescribed_density.txt")
+    # ========== theta grid ==========
+    xc, yc = np.array(thmo['xc']), np.array(thmo['yc']), # theta level (i, ), (j, )
+    zc = np.array(thmo['zc']) # theta level (k, )
+    denc = np.loadtxt("../constants/{CN}_density.txt".format(CN=case_name)) # theta level (k, )
+    zz = np.loadtxt("../constants/{CN}_z.txt".format(CN=case_name), skiprows=2)[:-1, 1] # W level (k, )
 
     set_axis = {
     "xmin": 35000, 
@@ -239,28 +232,29 @@ if __name__ == "__main__":
     xc = xc[set_axisarg["argxmin"]:set_axisarg["argxmax"]+1]
     yc = yc[set_axisarg["argymin"]:set_axisarg["argymax"]+1]
     zc = zc[set_axisarg["argzmin"]:set_axisarg["argzmax"]+1]
-    density = density[set_axisarg["argzmin"]:set_axisarg["argzmax"]+1]
-    czc = (zc[1:] + zc[:-1]) / 2
-    cdensity = (density[1:] + density[:-1]) / 2
-
-    #print("Dimension: {X} (X grids) x {Y} (Y grids) x {Z} (Z grids)" \
-    #      .format(X=len(xc), Y=len(yc), Z=len(zc)))
+    zz = zz[set_axisarg["argzmin"]:set_axisarg["argzmax"]+1]
+    denc = denc[set_axisarg["argzmin"]:set_axisarg["argzmax"]+1]
+    dx, dy, dz = np.gradient(xc), np.gradient(yc), np.gradient(zc) # (i, ), (j, ), (k, )
+    # boundary of theta (i+1, j+1, k+1)
+    xb = (np.array([xc[0] - dx[0]] + [x for x in xc]) + np.array([x for x in xc] + [xc[-1] + dx[-1]])) / 2 
+    yb = (np.array([yc[0] - dy[0]] + [y for y in yc]) + np.array([y for y in yc] + [yc[-1] + dy[-1]])) / 2 
+    zb = np.array([zc[0], (zc[0] + zc[1]) / 2] + [z for z in zz[1:]])
 
     for i in range(tidx, tidx+length):
         thmo = load_data(case_name, "Thermodynamic", idx=i)
         dyna = load_data(case_name, "Dynamic", idx=i)
-
+        
         # =====testing region=====
-
+        
         # =====testing region=====
-
+        
         # ========== drawing options ========== #
-        draw_cloud(profile=42000)
-        draw_buoyancy(profile=42000, type="tv")
-        draw_buoyancy(profile=42000, type="None")
-        draw_srf_rain()
-        draw_cwv()
-        draw_xycoreshell()
+        #draw_cloud(profile=42000)
+        #draw_buoyancy(profile=42000, type="tv")
+        #draw_buoyancy(profile=42000, type="None")
+        #draw_srf_rain()
+        #draw_wcv()
+        #draw_xycoreshell()
         draw_yzcoreshell(profile=42000)
         output_string = "task {START} -> {NOW} -> {END} ({P} %)"\
                         .format(START=tidx, NOW=i, END=tidx+length-1, P=((i - tidx + 1)/(length)*100))
